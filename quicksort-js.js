4d7ee92461a33a04e88369574e4d7242905157d2
mxrco <yosoymarco3@gmail.com>
2020-12-31 09:17:10 -0600

    quicksort

---
+++ quicksort-js.js
@@ -0,0 +1,13 @@
+function quicksort(arr) {
+    if(arr.length <= 1) return arr
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+    for(let i=1; i <= arr.length-1; i++){
+        pivot > arr[i] ? left.push(arr[i]) : right.push(arr[i]);
+    }
+    return [...quicksort(left), pivot, ...quicksort(right)]
+}
+
+arr = [9,5,4,0,1,45,30];
+console.log(quicksort(arr));
